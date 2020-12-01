bb236c483ddf6b7e77363746796546b548bf1547
Louis Leffler <louisleffler@gmail.com>
2020-12-01 03:16:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
-      binding.pry
       names.each do |name|
-        
-    end 
+       binding.pry 
+     end 
+   end 
     final_array
   end 
   binding.pry
